193e3a56963c34e4af0f93d76e1c07b71d12f353
sebastianbastian12 <sebastian.oliveros50@gmail.com>
2025-01-30 09:13:49 -0500

    "Adding inventory code"

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require('./controllers/baseController');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const env = require('dotenv').config();
 const app = express();
 const static = require('./routes/static');
+const inventoryRoute = require('./routes/inventoryRoute');
 
 /* ***************************
  * View Engine and Templates *
@@ -24,9 +26,10 @@ app.set('layout', './layouts/layout'); // not at views root
 app.use(static);
 
 //Index route
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Home' });
-});
+app.get('/', baseController.buildHome);
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
